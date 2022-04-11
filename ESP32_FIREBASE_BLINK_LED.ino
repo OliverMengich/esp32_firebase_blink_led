101fe94775536dbfd87d87d7458eb97caf5c2473
OliverMengich <kemeioliver2000@gmail.com>
2022-04-11 17:33:40 +0300

    working with esp32 and firebase to blink led

---
+++ ESP32_FIREBASE_BLINK_LED.ino
@@ -0,0 +1,91 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "Hotspot"
+#define WIFI_PASSWORD "1231231234"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyD_hyKEKP59DaFqfW1Sr6y1MCKDOUvYsXY"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://esp32firebasedemo-6dcf2-default-rtdb.firebaseio.com/" 
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+void setup(){
+  pinMode(5,OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok Connected to the Database");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Firebase.ready()){
+    // Write an string number on the database path LED/bool
+    digitalWrite(5, HIGH);
+    if (Firebase.RTDB.set(&fbdo, "LED/bool", true)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    delay(1000);
+    digitalWrite(5, LOW);
+    if (Firebase.RTDB.set(&fbdo, "LED/bool", false)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    delay(1000);
+  }
+}
